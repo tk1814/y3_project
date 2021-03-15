e5f9416f0643626ef5cef33913de131d6aa3ff36
Theodora Konstantinou <theodora.konstantinou@student.manchester.ac.uk>
2021-03-15 23:54:31 +0200

    1.3 js unit tests 100% solidity coverage

--- test/TestCredentialStore.js
+++ test/TestCredentialStore.js
@@ -6,11 +6,11 @@ const { expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
 contract("Blockchain Credential Store tests", async accounts => {
 
   let instance;
-  let image_hash_decoded_1 = bs58.decode('QmSB3NM8EXHAPkxBcaD4Y9Mx8AbDfAaGYo9JK2nut6ziM8').slice(2);
-  let image_hex_filename_1 = web3.utils.asciiToHex('testImageName1')
+  let image_hash_decoded = bs58.decode('QmSB3NM8EXHAPkxBcaD4Y9Mx8AbDfAaGYo9JK2nut6ziM8').slice(2);
+  let image_hex_filename = web3.utils.asciiToHex('testImageName1')
 
-  let file_hash_decoded_1 = bs58.decode('QmahGZvmav1s65vR6HEiL9YU2VwfzEQKU4WKXu4jg9HjEW').slice(2);
-  let file_hex_filename_1 = web3.utils.asciiToHex('testFileName1')
+  let file_hash_decoded = bs58.decode('QmahGZvmav1s65vR6HEiL9YU2VwfzEQKU4WKXu4jg9HjEW').slice(2);
+  let file_hex_filename = web3.utils.asciiToHex('testFileName1')
 
   beforeEach(function () {
     return CredStore.new()
@@ -23,15 +23,18 @@ contract("Blockchain Credential Store tests", async accounts => {
       });
   });
 
-  it("Test: user sign up - grant user role and get usernames", async () => {
+  it("Test: user sign up & login - grant user role and get usernames", async () => {
     let instance = await CredStore.deployed();
 
-    let userSignUp1 = await instance.signUpUserOrLogin('testUsername1', 'Thu Mar 11 2021 15:53:27 GMT+0200 (Eastern European Standard Time)', { from: accounts[0] });
-    expectEvent(userSignUp1, 'RoleGranted');
+    let userSignUp = await instance.signUpUserOrLogin('testUsername1', 'Thu Mar 11 2021 15:53:27 GMT+0200 (Eastern European Standard Time)', { from: accounts[0] });
+    expectEvent(userSignUp, 'RoleGranted');
 
     let expected = JSON.stringify(['testUsername1']);
     let actual = JSON.stringify(await instance.getUsernames());
     assert.equal(actual, expected);
+
+    await instance.signUpUserOrLogin('testUsername1', 'Thu Mar 13 2021 17:13:27 GMT+0200 (Eastern European Standard Time)', { from: accounts[0] });
+
   });
 
   it("Test: new user accepted terms and conditions", async () => {
@@ -60,19 +63,19 @@ contract("Blockchain Credential Store tests", async accounts => {
     });
 
     it("Test: revert state when sharing images without permission", async () => {
-      await expectRevert(instance.shareImage('testUsername5', accounts[1], image_hash_decoded_1, image_hex_filename_1, 'Thu Mar 12 2021 14:56:28 GMT+0200 (Eastern European Standard Time)', true, { from: accounts[5] }), 'Caller is not a user');
+      await expectRevert(instance.shareImage('testUsername5', accounts[1], image_hash_decoded, image_hex_filename, 'Thu Mar 12 2021 14:56:28 GMT+0200 (Eastern European Standard Time)', true, { from: accounts[5] }), 'Caller is not a user');
     });
 
     it("Test: revert state when sharing files without permission", async () => {
-      await expectRevert(instance.shareFile('testUsername5', accounts[1], file_hash_decoded_1, file_hex_filename_1, 'Thu Mar 12 2021 14:56:28 GMT+0200 (Eastern European Standard Time)', true, { from: accounts[5] }), 'Caller is not a user');
+      await expectRevert(instance.shareFile('testUsername5', accounts[1], file_hash_decoded, file_hex_filename, 'Thu Mar 12 2021 14:56:28 GMT+0200 (Eastern European Standard Time)', true, { from: accounts[5] }), 'Caller is not a user');
     });
 
     it("Test: revert state when uploading images without permission", async () => {
-      await expectRevert(instance.setImage(image_hash_decoded_1, image_hex_filename_1, 'Thu Mar 12 2021 14:56:28 GMT+0200 (Eastern European Standard Time)', { from: accounts[5] }), 'Caller is not a user');
+      await expectRevert(instance.setImage(image_hash_decoded, image_hex_filename, 'Thu Mar 12 2021 14:56:28 GMT+0200 (Eastern European Standard Time)', { from: accounts[5] }), 'Caller is not a user');
     });
 
     it("Test: revert state when uploading files without permission", async () => {
-      await expectRevert(instance.setFile(file_hash_decoded_1, file_hex_filename_1, 'Thu Mar 13 2021 16:16:38 GMT+0200 (Eastern European Standard Time)', { from: accounts[5] }), 'Caller is not a user');
+      await expectRevert(instance.setFile(file_hash_decoded, file_hex_filename, 'Thu Mar 13 2021 16:16:38 GMT+0200 (Eastern European Standard Time)', { from: accounts[5] }), 'Caller is not a user');
     });
   });
 
@@ -80,7 +83,7 @@ contract("Blockchain Credential Store tests", async accounts => {
 
     it("Test: store and retrieve an image", async () => {
 
-      await instance.setImage(image_hash_decoded_1, image_hex_filename_1, 'Thu Mar 12 2021 14:56:28 GMT+0200 (Eastern European Standard Time)', { from: accounts[0] })
+      await instance.setImage(image_hash_decoded, image_hex_filename, 'Thu Mar 12 2021 14:56:28 GMT+0200 (Eastern European Standard Time)', { from: accounts[0] })
 
       let expected_image = JSON.stringify({ "0": ["0x38f88f838cc13b915ddd8a9573c034a303bc7c32f03b007766f2233f06c778eb"], "1": ["0x74657374496d6167654e616d6531000000000000000000000000000000000000"], "2": "testUsername1", "3": ["Thu Mar 12 2021 14:56:28 GMT+0200 (Eastern European Standard Time)"], "4": [], "5": [], "6": "Thu Mar 11 2021 15:53:27 GMT+0200 (Eastern European Standard Time)", "7": [] })
       let actual_image = JSON.stringify(await instance.getImages());
@@ -90,7 +93,7 @@ contract("Blockchain Credential Store tests", async accounts => {
 
     it("Test: store and retrieve a file", async () => {
 
-      await instance.setFile(file_hash_decoded_1, file_hex_filename_1, 'Thu Mar 13 2021 16:16:38 GMT+0200 (Eastern European Standard Time)', { from: accounts[0] })
+      await instance.setFile(file_hash_decoded, file_hex_filename, 'Thu Mar 13 2021 16:16:38 GMT+0200 (Eastern European Standard Time)', { from: accounts[0] })
 
       let expected_file = JSON.stringify({ "0": ["0xb79467ba150720b5139ecefb6eb82541f8ccae0ded2b5aa25c5153254a76c757"], "1": ["0x7465737446696c654e616d653100000000000000000000000000000000000000"], "2": "testUsername1", "3": ["Thu Mar 13 2021 16:16:38 GMT+0200 (Eastern European Standard Time)"], "4": [], "5": [], "6": [] });
       let actual_file = JSON.stringify(await instance.getFiles());
@@ -104,7 +107,7 @@ contract("Blockchain Credential Store tests", async accounts => {
 
     it("Test: share and retrieve a shared image", async () => {
 
-      await instance.shareImage('testUsername1', accounts[1], image_hash_decoded_1, image_hex_filename_1, 'Thu Mar 12 2021 14:56:28 GMT+0200 (Eastern European Standard Time)', true, { from: accounts[0] })
+      await instance.shareImage('testUsername1', accounts[1], image_hash_decoded, image_hex_filename, 'Thu Mar 12 2021 14:56:28 GMT+0200 (Eastern European Standard Time)', true, { from: accounts[0] })
 
       let expected_shared_image = JSON.stringify({ "0": ["0x38f88f838cc13b915ddd8a9573c034a303bc7c32f03b007766f2233f06c778eb"], "1": ["0x74657374496d6167654e616d6531000000000000000000000000000000000000"], "2": [accounts[0]], "3": ["testUsername1"], "4": ["Thu Mar 12 2021 14:56:28 GMT+0200 (Eastern European Standard Time)"], "5": [true] })
       let actual_shared_image = JSON.stringify(await instance.getSharedImageArr({ from: accounts[1] }));
@@ -114,7 +117,7 @@ contract("Blockchain Credential Store tests", async accounts => {
 
     it("Test: share and retrieve a shared file", async () => {
 
-      await instance.shareFile('testUsername1', accounts[1], file_hash_decoded_1, file_hex_filename_1, 'Thu Mar 12 2021 14:56:28 GMT+0200 (Eastern European Standard Time)', true, { from: accounts[0] })
+      await instance.shareFile('testUsername1', accounts[1], file_hash_decoded, file_hex_filename, 'Thu Mar 12 2021 14:56:28 GMT+0200 (Eastern European Standard Time)', true, { from: accounts[0] })
 
       let expected_shared_file = JSON.stringify({ "0": ["0xb79467ba150720b5139ecefb6eb82541f8ccae0ded2b5aa25c5153254a76c757"], "1": ["0x7465737446696c654e616d653100000000000000000000000000000000000000"], "2": [accounts[0]], "3": ["testUsername1"], "4": ["Thu Mar 12 2021 14:56:28 GMT+0200 (Eastern European Standard Time)"], "5": [true] })
       let actual_shared_file = JSON.stringify(await instance.getSharedFileArr({ from: accounts[1] }));
@@ -128,7 +131,7 @@ contract("Blockchain Credential Store tests", async accounts => {
 
     it("Test: retrieve images the user shared with others", async () => {
 
-      await instance.shareImage('testUsername1', accounts[1], image_hash_decoded_1, image_hex_filename_1, 'Thu Mar 12 2021 14:56:28 GMT+0200 (Eastern European Standard Time)', false, { from: accounts[0] })
+      await instance.shareImage('testUsername1', accounts[1], image_hash_decoded, image_hex_filename, 'Thu Mar 12 2021 14:56:28 GMT+0200 (Eastern European Standard Time)', false, { from: accounts[0] })
 
       let expected_shared_images = JSON.stringify({ "0": [], "1": [], "2": "testUsername1", "3": [], "4": [accounts[1]], "5": ["0x38f88f838cc13b915ddd8a9573c034a303bc7c32f03b007766f2233f06c778eb"], "6": "Thu Mar 11 2021 15:53:27 GMT+0200 (Eastern European Standard Time)", "7": ["0x74657374496d6167654e616d6531000000000000000000000000000000000000"] })
       let actual_shared_images = JSON.stringify(await instance.getImages({ from: accounts[0] }));
@@ -138,7 +141,7 @@ contract("Blockchain Credential Store tests", async accounts => {
 
     it("Test: retrieve files the user shared with others", async () => {
 
-      await instance.shareFile('testUsername1', accounts[1], file_hash_decoded_1, file_hex_filename_1, 'Thu Mar 12 2021 14:26:27 GMT+0200 (Eastern European Standard Time)', false, { from: accounts[0] })
+      await instance.shareFile('testUsername1', accounts[1], file_hash_decoded, file_hex_filename, 'Thu Mar 12 2021 14:26:27 GMT+0200 (Eastern European Standard Time)', false, { from: accounts[0] })
 
       let expected_shared_file = JSON.stringify({ "0": [], "1": [], "2": "testUsername1", "3": [], "4": [accounts[1]], "5": ["0xb79467ba150720b5139ecefb6eb82541f8ccae0ded2b5aa25c5153254a76c757"], "6": ["0x7465737446696c654e616d653100000000000000000000000000000000000000"] })
       let actual_shared_files = JSON.stringify(await instance.getFiles({ from: accounts[0] }));
