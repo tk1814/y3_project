74deec5cbce91be36d9222fe661962541e1af68e
Theodora Konstantinou <theodora.konstantinou@student.manchester.ac.uk>
2021-03-26 09:35:56 +0200

    About.js, css, code count changes

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -19,9 +19,13 @@ class About extends Component {
 
         <div className="middle_about_space"></div>
 
-        <h3 className="mt-3">1. Log in to your Metamask account </h3>
-        <h3 className="mt-3">2.Enter your username to sign up</h3>
-        <h3 className="mt-3">3.Upload and share your files </h3>
+        <div className="mt-3 h8">1. Download the Metamask plugin <a style={{color: '#6e967a'}} href='https://metamask.io/download'>here</a> </div>
+        <div className="mt-3 h8">2. Log in to your Metamask account </div>
+        <div className="mt-3 h8">3. Add funds to your account </div>
+        <div className="mt-3 h8">4. Enter your username to sign up</div>
+        <div className="mt-3 h8">5. You are ready to upload and share your files </div>
+        {/* <h3 className="mt-3"></h3> */}
+
 
       </div >
     );
